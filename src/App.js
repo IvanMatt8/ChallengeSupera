6cfd72684fe0cdcffbc3d25935a57f580229330e
Ivan Matheus <ivanmatt1995@gmai.com>
2021-10-06 20:20:50 -0400

    :sparkles: Passando Context ao redor das Rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
+import { StoreContent } from "./hooks/useProducts.js";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <StoreContent>
+        <Routes />
+      </StoreContent>
     </BrowserRouter>
   );
 }
