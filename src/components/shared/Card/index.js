3de832dd0ba0d8383c7f007df14cc47a4506685d
Ivan Matheus <ivanmatt1995@gmai.com>
2021-10-06 20:19:19 -0400

    :sparkles: Add Logica de adicionar ao carrinho e remover,  incrementar e decrementar quantidade

--- src/components/shared/Card/index.js
+++ src/components/shared/Card/index.js
@@ -1,15 +1,62 @@
 import React from "react";
 import "./style.css";
-import MortalKombat from "../../../assets/mortal-kombat-xl.png";
-import { FiPlusCircle } from "react-icons/fi";
+import { FiTrash } from "react-icons/fi";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { useStoreContent } from "../../../hooks/useProducts.js";
 
-function Card() {
+function Card({ isCart, image, name, score, price, id, quantidade }) {
+  const {
+    handleClickBadge,
+    handleRemoveClickBadge,
+    handleIncrementAndDecrementQuantidadeByID,
+  } = useStoreContent();
+
   return (
     <div className="card">
-      <FiPlusCircle className="carrinhoCard" size={25} />
-      <img src={MortalKombat} alt="imagem Jogos" />
-      <h3>Mortal Kombat</h3>
-      <span>R$ 139,00</span>
+      <div className="headerCard">
+        {isCart ? (
+          <div className="wrapperButtonCard">
+            <button
+              onClick={() =>
+                handleIncrementAndDecrementQuantidadeByID(id, "desc")
+              }
+            >
+              <h4>-</h4>
+            </button>
+            <h4>{quantidade}</h4>
+            <button
+              onClick={() =>
+                handleIncrementAndDecrementQuantidadeByID(id, "asc")
+              }
+            >
+              <h4>+</h4>
+            </button>
+          </div>
+        ) : (
+          <div className="wrapperButtonCard" style={{ border: 0 }} />
+        )}
+        <div className="borderIcon">
+          {isCart ? (
+            <button onClick={() => handleRemoveClickBadge(id)}>
+              <FiTrash className="icon" />
+            </button>
+          ) : (
+            <button
+              onClick={() =>
+                handleClickBadge({ id, image, name, score, price })
+              }
+            >
+              <HiOutlineShoppingBag className="icon" />
+            </button>
+          )}
+        </div>
+      </div>
+
+      <div className="wrapperContentCard">
+        <img src={image} alt="imagem Jogos" />
+        <h3>{name}</h3>
+        <span>R$ {price}</span>
+      </div>
     </div>
   );
 }
